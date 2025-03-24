e9258305192046b75dd6fef9e26654015704a62e
Nikanora <olga.nikanorova@gmail.com>
2025-03-24 19:12:53 +0000

    feat: solve task 'Returns the date of the next Friday from a given date'

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -51,8 +51,8 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  return new Date(date).toLocaleDateString('en-US', { weekday: 'long' });
 }
 
 /**
@@ -66,8 +66,12 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const newDate = new Date(date);
+  const currentDay = newDate.getDay();
+  const daysUntilFri = (5 - currentDay + 7) % 7 || 7;
+  newDate.setDate(newDate.getDate() + daysUntilFri);
+  return newDate;
 }
 
 /**
